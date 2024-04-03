eeaa5bd9c57faf1186ac35037c5f62bff8801bb0
ngrogg <oddiusoddone@gmail.com>
2024-04-03 00:12:24 +0000

    parser.pl. Fleshed out main function and added TODO comments to remaining functions.

--- parser.pl
+++ parser.pl
@@ -12,18 +12,35 @@ use XML::Simple;
 sub helpFunction {
     print "Help";
 	print "----------------------------------------------------\n";
+
+    ## TODO: Help output
 }
 
 # XML to JSON function
 sub xmlToJson {
     print "XML -> JSON";
 	print "----------------------------------------------------\n";
+
+    ## Read passed values
+    my ($command,$inputFile,$outputFile) = @_;
+
+    ## TODO: Validation
+
+    ## TODO: Parse XML to JSON
+
 }
 
 # JSON to XML function
 sub jsonToXml {
     print "JSON -> XML";
 	print "----------------------------------------------------\n";
+
+    ## Read passed values
+    my ($command,$inputFile,$outputFile) = @_;
+
+    ## TODO: Validation
+
+    ## TODO: Parse JSON to XML
 }
 
 # Function to run program
@@ -32,12 +49,32 @@ sub runProgram {
 	print "----------------------------------------------------\n";
 
     ## Parse passed arguments
+    my ($option,$file1,$file2) = @ARGV;
 
     ## Take action depending on passed arguments
-    ### If $1 == 'help' run help function and exit
-    ### If input XML and output JSON, run XML -> JSON function
-    ### If input JSON and output JSON, run JSON to XML function
-    ### Else invalid options passed, run help function and exit
+    switch($option) {
+        ### If $option == 'help' run help function and exit
+        case "help" {
+                helpFunction();
+                exit 0;
+        }
+        ### If input XML and output JSON, run XML -> JSON function
+        case "xml" {
+                xmlToJson($option,$file1,$file2);
+                exit 0;
+        }
+        ### If input JSON and output JSON, run JSON to XML function
+        case "json" {
+                jsonToXml($option,$file1,$file2);
+                exit 0;
+        }
+        ### Else invalid options passed, run help function and exit
+        else {
+                print "Invalid option passed, running help function and exiting!";
+                helpFunction();
+                exit 1;
+        }
+    }
 }
 
 # Run the function
