6abfbce5a1c136961ada5bd0d95d6dcdfdb2e12e
Nicholas Grogg <ngrogg@pop-os.localdomain>
2024-04-06 23:25:32 -1000

    parser.pl. Changed given/when to switch/case after installing Switch. README.md. Updated README.md with required Perl packages and the statement to install them.

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ An in-progress Perl script for moving XML files to the JSON format or vice-versa
 Parser script requires the following modules: <br>
 * JSON
 * XML::Simple
+* Switch
 
-Both can be installed with the cpan command: <br>
+Packages can be installed with the cpan command: <br>
 `cpan install JSON` <br>
 `cpan install XML::Simple` <br>
+`cpan install Switch` <br>
 
 ## Usage
 Takes an input file, format, output file, format and output filepath as arguments. <br>
--- parser.pl
+++ parser.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
-use feature qw(switch);
+use Switch;
+#use feature qw(switch);
 use warnings;
 use strict;
 use JSON;
@@ -13,9 +14,8 @@ use XML::Simple;
 sub helpFunction {
     print "Help\n";
 	print "----------------------------------------------------\n";
-
     ## TODO: Help output
-}
+    }
 
 # XML to JSON function
 sub xmlToJson {
@@ -53,24 +53,29 @@ sub runProgram {
     my ($option,$file1,$file2) = @ARGV;
 
     ## Take action depending on passed arguments
-    given ($option) {
+    #given ($option) {
+    switch($option) {
         ### If $option == 'help' run help function and exit
-        when ("help") {
+        #when ("help") {
+        case "help" {
                 helpFunction();
                 exit 0;
         }
         ### If input XML and output JSON, run XML -> JSON function
-        when ("xml") {
+        #when ("xml") {
+        case "xml" {
                 xmlToJson($option,$file1,$file2);
                 exit 0;
         }
         ### If input JSON and output JSON, run JSON to XML function
-        when ("json") {
+        #when ("json") {
+        case "json" {
                 jsonToXml($option,$file1,$file2);
                 exit 0;
         }
         ### Else invalid options passed, run help function and exit
-        default {
+        #default {
+        else {
                 print "Invalid option passed, running help function and exiting!\n";
                 helpFunction();
                 exit 1;
