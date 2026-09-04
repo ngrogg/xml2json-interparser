d0dccc7b5926cff1d391061a8dc267a58d847ee0
Nicholas Grogg <Nicholas_Grogg6505@outlook.com>
2026-09-04 13:08:20 -1000

    parser.pl. Complete refactor.

--- parser.pl
+++ parser.pl
@@ -1,15 +1,17 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
+
+# Generic Perl imports
 use warnings;
 use strict;
+
+# Script specific imports
+use File::Spec;
 use JSON;
-use JSON::Validator;
-use Switch;
-#use feature qw(switch);
 use XML::Simple;
 use XML::LibXML;
 
 # xml2json-interparser
-# Move XML and JSON files between formatting
+# Perl script to move XML and JSON files between formatting
 # By Nicholas Grogg
 
 # Help
@@ -17,257 +19,239 @@ sub helpFunction {
     print "Help\n";
 	print "----------------------------------------------------\n";
     print "XML 2 JSON Interparser\n";
-    print "Move XML and JSON files between formats\n";
-    print "Takes a format to convert to, an input file, output file and destination path as arguments\n";
-    print "If no output path is provided default ~/\n";
-    print "Usage. ./parser.pl format inFile outFile /path/to/outputFolder\n";
-    print "Ex. ./parserl.pl xml input.xml output.json /home/jsmith/Documents\n";
+    print "Move XML and JSON files between formats\n\n";
+    print " \n";
+    print "Usage:\n";
+    print " ./parser.pl format inputFile outputFile [outputPath]\n\n";
+    print " \n";
+    print "Formats:\n";
+    print " xml Convert XML to JSON\n";
+    print " json Convert JSON to XML\n";
+    print " help Display this help message\n\n";
+    print " \n";
+    print "Arguments:\n";
+    print " format Conversion format\n";
+    print " inputFile File to convert\n";
+    print " outputFile Name of the converted file\n";
+    print " outputPath Destination directory (default: \$HOME)\n\n";
+    print " \n";
+    print "Examples:\n";
+    print " ./parser.pl xml input.xml output.json\n";
+    print " ./parser.pl xml input.xml output.json /home/jsmith/Documents\n";
+    print " ./parser.pl json input.json output.xml\n";
 }
 
-# XML File Validator, checks that provided XML files is valid
-sub xmlValidation{
-        ## Assign passed XML file to variable
-        my ($xmlFile) = @_;
+# Validate common arguments
+sub validateArguments {
+    my ($inputFile, $outputFile) = @_;
 
-        ## Create XML Parser object
-        my $parser = XML::LibXML->new();
+    ## Check that an input filename was provided
+    if (!defined $inputFile || $inputFile eq '') {
+        die "ISSUE DETECTED: An input filename was not provided!\n";
+    }
 
-        # Validate XML file
-        eval{
-                my $validator = $parser->parse_file($xmlFile);
-        };
+    ## Check that an output filename was provided
+    if (!defined $outputFile || $outputFile eq '') {
+        die "ISSUE DETECTED: An output filename was not provided!\n";
+    }
 
-        # If validator returns false, return 0
-        if ($@){
-                return 0;
-        }
-        # Else return 1
-        else {
-                return 1;
-        }
+    ## Check that the input file exists
+    if (!-e $inputFile) {
+        die "ISSUE DETECTED: Input file '$inputFile' does not exist!\n";
+    }
+
+    ## Check that the input file is readable
+    if (!-r $inputFile) {
+        die "ISSUE DETECTED: Input file '$inputFile' is not readable!\n";
+    }
 }
 
-# JSON File Validator, checks that provided JSON file is valid
-sub jsonValidation{
-        # Assign passed JSON file to variable
-        my ($jsonFile) = @_;
 
-        ## Create JSON Validator object
-        my $validator = JSON::Validator->new();
+# Build output file path
+sub buildOutputPath {
+    my ($outputFile, $outputFilePath) = @_;
 
-        ## Validate JSON file
-        eval {
-                $validator->validate($jsonFile);
-        };
+    ## If no output path was provided, use the user's home directory
+    if (!defined $outputFilePath || $outputFilePath eq '') {
+        $outputFilePath = $ENV{HOME};
 
-        ## If validator returns false, return 0
-        if($@){
-                return 0;
+        if (!defined $outputFilePath || $outputFilePath eq '') {
+            die "ISSUE DETECTED: Unable to determine user's home directory!\n";
         }
-        ## Else return 1
-        else{
-                return 1;
-        }
+    }
+
+    ## Build the complete output path
+    return File::Spec->catfile($outputFilePath, $outputFile);
 }
+
+
+# XML File Validator
+# Checks whether the provided XML file can be parsed
+sub xmlValidation {
+    my ($xmlFile) = @_;
+
+    ## Create XML parser object
+    my $parser = XML::LibXML->new();
+
+    ## Attempt to parse XML file
+    eval {
+        $parser->parse_file($xmlFile);
+    };
+
+    ## Return false if parsing failed
+    if ($@) {
+        return 0;
+    }
+
+    ## XML parsed successfully
+    return 1;
+}
+
 
 # XML to JSON function
 sub xmlToJson {
     print "XML -> JSON\n";
-	print "----------------------------------------------------\n";
+    print "----------------------------------------------------\n";
 
     ## Read passed values
-    my ($inputFile,$outputFile,$outputFilePath) = @_;
+    my ($inputFile, $outputFile, $outputFilePath) = @_;
 
-    ## Validation
-    ### Is inputFile null?
-    if (not defined $inputFile){
-        print "ISSUE DETECTED: An input filename was not found!\n";
-	    print "----------------------------------------------------\n";
-        print "Re-run script with valid values. \n";
-        exit 1;
+    ## Validate common arguments
+    validateArguments($inputFile, $outputFile);
+
+    ## Validate XML input file
+    if (xmlValidation($inputFile)) {
+        print "XML input file is valid, continuing.\n";
     }
     else {
-        print "Input filename was defined\n";
-	    print "----------------------------------------------------\n";
-        print "Proceeding\n";
+        die "ISSUE DETECTED: XML input file failed to validate!\n";
     }
 
-    ### Is outputFile null?
-    if (not defined $outputFile){
-        print "ISSUE DETECTED: An output filename was not found!\n";
-	    print "----------------------------------------------------\n";
-        print "Re-run script with valid values. \n";
-        exit 1;
-    }
-    else {
-        print "Output Filename was defined\n";
-	    print "----------------------------------------------------\n";
-        print "Proceeding\n";
-    }
+    ## Build complete output path
+    my $outputPath = buildOutputPath($outputFile, $outputFilePath);
 
-    ### Is outputFilePath null? If null use default, else use provided filepath
-    if (not defined $outputFilePath){
-        print "IMPORTANT: Filepath for output file undefined!\n";
-	    print "----------------------------------------------------\n";
-        print "Defaulting to ~/\n";
-        print "Press enter to proceed or control-c to quit\n";
-        my $junkInput = <STDIN>;
-    }
-    else {
-        print "Output Filepath was defined\n";
-	    print "----------------------------------------------------\n";
-        print "Proceeding\n";
-    }
+    ## Read XML file
+    open my $xmlRead, '<:encoding(UTF-8)', $inputFile
+        or die "Unable to open $inputFile: $!";
 
-    ### Is provided XML file valid?
-    if (xmlValidation($inputFile)){
-            print "XML input file valid, continuing.\n";
-    }
-    else {
-            print "ISSUE DETECTED: XML input file failed to validate!\n";
-	        print "----------------------------------------------------\n";
-            print "Verify XML file and re-run script!\n";
-            exit 1;
-    }
+    my $xmlText = do {
+        local $/;
+        <$xmlRead>;
+    };
 
-    ## Parse XML to JSON
-    ### Create XML items
-    my $xmlInput   = $inputFile;
-    my $jsonOutput = $outputFile;
+    close $xmlRead
+        or die "Unable to close $inputFile: $!";
 
-    ### Read in XML files
-    open my $xmlRead, '<', $xmlInput or die "Unable to open $inputFile: $!";
-    my $xmlText    = do { local $/; <$xmlRead> };
-    close $xmlRead;
+    ## Convert XML data to Perl data structure
+    my $xmlData = XMLin($xmlText);
 
-    ### Convert XML Data to Perl data structure
-    my $xmlData    = XMLin($xmlText);
+    ## Convert Perl data structure to JSON
+    my $jsonText = to_json($xmlData, { pretty => 1 });
 
-    ### Convert Perl data structure to JSON data
-    my $jsonText    = to_json($xmlData, { pretty => 1});
+    ## Write JSON data to file
+    open my $jsonWrite, '>:encoding(UTF-8)', $outputPath
+        or die "Unable to open $outputPath: $!";
 
-    ### Write JSON data to file
-    open my $jsonWrite, '>', $jsonOutput or die "Unable to open $jsonOutput: $!";
-    print $jsonWrite $jsonText;
-    close $jsonWrite;
+    print $jsonWrite $jsonText
+        or die "Unable to write to $outputPath: $!";
 
+    close $jsonWrite
+        or die "Unable to close $outputPath: $!";
+
+    print "Conversion complete.\n";
+    print "Output: $outputPath\n";
 }
+
 
 # JSON to XML function
 sub jsonToXml {
     print "JSON -> XML\n";
-	print "----------------------------------------------------\n";
+    print "----------------------------------------------------\n";
 
     ## Read passed values
-    my ($inputFile,$outputFile,$outputFilePath) = @_;
+    my ($inputFile, $outputFile, $outputFilePath) = @_;
 
-    ## Validation
-    ### Is inputFile null?
-    if (not defined $inputFile){
-        print "ISSUE DETECTED: An input filename was not found!\n";
-	    print "----------------------------------------------------\n";
-        print "Re-run script with valid values. \n";
-        exit 1;
-    }
-    else {
-        print "Input filename was defined\n";
-	    print "----------------------------------------------------\n";
-        print "Proceeding\n";
-    }
+    ## Validate common arguments
+    validateArguments($inputFile, $outputFile);
 
-    ### Is outputFile null?
-    if (not defined $outputFile){
-        print "ISSUE DETECTED: An output filename was not found!\n";
-	    print "----------------------------------------------------\n";
-        print "Re-run script with valid values. \n";
-        exit 1;
-    }
-    else {
-        print "Output Filename was defined\n";
-	    print "----------------------------------------------------\n";
-        print "Proceeding\n";
-    }
+    ## Build complete output path
+    my $outputPath = buildOutputPath($outputFile, $outputFilePath);
 
-    ### Is outputFilePath null? If null use default, else use provided filepath
-    if (not defined $outputFilePath){
-        print "IMPORTANT: Filepath for output file undefined!\n";
-	    print "----------------------------------------------------\n";
-        print "Defaulting to ~/\n";
-        print "Press enter to proceed or control-c to quit\n";
-        my $junkInput = <STDIN>;
-    }
-    else {
-        print "Output Filepath was defined\n";
-	    print "----------------------------------------------------\n";
-        print "Proceeding\n";
+    ## Read JSON file
+    open my $jsonRead, '<:encoding(UTF-8)', $inputFile
+        or die "Unable to open $inputFile: $!";
+
+    my $jsonData = do {
+        local $/;
+        <$jsonRead>;
+    };
+
+    close $jsonRead
+        or die "Unable to close $inputFile: $!";
+
+    ## Convert JSON data to Perl data structure.
+    ## decode_json() will throw an error if the JSON is invalid.
+    my $perlData;
+
+    eval {
+        $perlData = decode_json($jsonData);
+    };
+
+    if ($@) {
+        die "ISSUE DETECTED: JSON input file failed to validate!\n";
     }
 
+    ## Convert Perl data structure to XML
+    my $xmlData = XMLout(
+        $perlData,
+        Rootname => 'root',
+        XMLDecl  => 1
+    );
 
-    ### Is provided JSON file valid?
-    if (jsonValidation($inputFile)){
-            print "JSON input file valid, continuing.\n";
-    }
-    else {
-            print "ISSUE DETECTED: JSON input file failed to validate!\n";
-	        print "----------------------------------------------------\n";
-            print "Verify JSON file and re-run script!\n";
-            exit 1;
-    }
+    ## Write XML data to file
+    open my $xmlWrite, '>:encoding(UTF-8)', $outputPath
+        or die "Unable to open $outputPath: $!";
 
-    ## Parse JSON to XML
-    ### Create JSON items
-    my $jsonInput = $inputFile;
-    my $xmlOutput = $outputFile;
+    print $xmlWrite $xmlData
+        or die "Unable to write to $outputPath: $!";
 
-    ### Read in JSON files
-    open my $jsonRead, '<', $jsonInput or die "Unable to open $jsonInput: $!";
-    my $jsonData  = do { local $/; <$jsonRead> };
-    close $jsonRead;
+    close $xmlWrite
+        or die "Unable to close $outputPath: $!";
 
-    ### Convert JSON data to Perl data structure
-    my $perlData  = decode_json($jsonData);
-
-    ### Convert Perl data structure to XML
-    my $xmlData   = XMLout($perlData, Rootname => 'root', XMLDecl => 1);
-
-    ### Write XML to file
-    open my $xmlWrite, '>', $xmlOutput or die "Unable to open $xmlOutput: $!";
-    print $xmlWrite $xmlData;
-    close $xmlWrite;
-
+    print "Conversion complete.\n";
+    print "Output: $outputPath\n";
 }
+
 
 # Function to run program
 sub runProgram {
-	print "XML To JSON interparser\n";
-	print "----------------------------------------------------\n";
+    print "XML To JSON interparser\n";
+    print "----------------------------------------------------\n";
 
     ## Parse passed arguments
-    my ($option,$file1,$file2,$outFilePath) = @ARGV;
+    my ($option, $file1, $file2, $outFilePath) = @ARGV;
+
+    ## Display help if no option was provided
+    if (!defined $option) {
+        helpFunction();
+        exit 1;
+    }
 
     ## Take action depending on passed arguments
-    switch($option) {
-        ### If $option == 'help' run help function and exit
-        case "help" {
-                helpFunction();
-                exit 0;
-        }
-        ### If input XML and output JSON, run XML -> JSON function
-        case "xml" {
-                xmlToJson($file1,$file2,$outFilePath);
-                exit 0;
-        }
-        ### If input JSON and output XML, run JSON to XML function
-        case "json" {
-                jsonToXml($file1,$file2,$outFilePath);
-                exit 0;
-        }
-        ### Else invalid options passed, run help function and exit
-        else {
-                print "Invalid option passed, running help function and exiting!\n";
-                helpFunction();
-                exit 1;
-        }
+    if ($option eq 'help') {
+        helpFunction();
+        exit 0;
+    }
+    elsif ($option eq 'xml') {
+        xmlToJson($file1, $file2, $outFilePath);
+    }
+    elsif ($option eq 'json') {
+        jsonToXml($file1, $file2, $outFilePath);
+    }
+    else {
+        print "Invalid option '$option' passed.\n\n";
+        helpFunction();
+        exit 1;
     }
 }
 
